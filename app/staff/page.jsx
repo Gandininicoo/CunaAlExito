5086752ba132d686f629972e83907301583d81bc
Nicolas Gandini Lopez <nicolasgandinilopez@gmail.com>
2024-07-25 02:01:48 -0300

    correciones 25/07

--- app/staff/page.jsx
+++ app/staff/page.jsx
@@ -7,7 +7,7 @@ export default function staff(){
             <StaffMembers Nombre={'Pablo Pereyra'} Rol={'Presidente'} Wpp={'+54 (341) 3886474'} Mail={'pablomartinpereyra@gmail.com'}/>
             <StaffMembers Nombre={'Santiago Iommi Cilea'} Rol={'Director Ejecutivo'} Wpp={'+54 (3465) 656192'} Mail={'santiagoiommi@hotmail.com'}/>
             <StaffMembers Nombre={'Angel Silva'} Rol={'Director Deportivo'} Wpp={'+54 (341) 5883010'} Mail={'angelari1977@hotmail.com'}/>
-            <StaffMembers Nombre={'Raul Merich'} Rol={'Area Contable'}/>
+            <StaffMembers Nombre={'Raul Medich'} Rol={'Area Contable'}/>
             <StaffMembers Nombre={'Juan Martin Van Poepelen'} Rol={'Abogado Deportivo'}/>
             <StaffMembers Nombre={'Matias Magri'} Rol={'Abogado Deportivo'} />
         </article>

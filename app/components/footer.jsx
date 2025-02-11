a51376e5d63d6a1092976f29860cd03778cdc262
Nicolas <nglwebdeveloper@gmail.com>
2025-02-11 19:37:47 -0300

    modificaciones 11-02

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,14 +2,11 @@ export default function Footer() {
     return (
         <footer>
             <section>
-            <div className="linkimg"><img src="../logo.png" alt="Regionales El Remanso Logo" /></div>
+            <div className="linkimg"><img src="../logo.png" alt="De la cuna al Exito logo" /></div>
             </section>
             <section>
             <a href={`https://wa.me/5403413886474?text=Hola Pablo me comunico para realizarle una consulta sobre "De la Cuna al Exito"`} target='_blank' rel="nofollow">
                 <span>+54 (341) 388-6474</span>
-            </a>
-            <a href={`https://wa.me/5403465656192?text=Hola Santiago me comunico para realizarle una consulta sobre "De la Cuna al Exito"`} target='_blank' rel="nofollow">
-                <span>+54 (3465) 65-6192</span>
             </a>
             <a className="header--custom-text" itemprop="email" href="mailto:" rel="nofollow">
                 <span></span>
